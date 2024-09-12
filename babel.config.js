a467b4928d00c503ec17ddefc3bca66a62ffc3f5
CongAnh2122003 <conganh2122003@gmail.com>
2024-09-12 13:22:08 +0700

    coding convention

--- babel.config.js
+++ babel.config.js
@@ -40,29 +40,14 @@ module.exports = function(api) {
       'babel-plugin-macros',
       '@babel/plugin-syntax-dynamic-import',
       isTestEnv && 'babel-plugin-dynamic-import-node',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object',
+      '@babel/plugin-proposal-class-properties',
       '@babel/plugin-transform-destructuring',
-      [
-        '@babel/plugin-proposal-class-properties',
-        {
-          loose: true
-        }
-      ],
       [
         '@babel/plugin-proposal-object-rest-spread',
         {
           useBuiltIns: true
-        }
-      ],
-      [
-        '@babel/plugin-proposal-private-methods',
-        {
-          loose: true
-        }
-      ],
-      [
-        '@babel/plugin-proposal-private-property-in-object',
-        {
-          loose: true
         }
       ],
       [
