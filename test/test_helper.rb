b5df7d719377a9d2aab86689b99ae82b8ceb0c6b
CongAnh2122003 <conganh2122003@gmail.com>
2024-09-19 16:57:02 +0700

    Chapter 10: Updating, showing, and deleting users

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,8 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors, with: :threads)
+  # parallelize(workers: :number_of_processors, with: :threads)
+  parallelize(workers: 1, with: :threads)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
@@ -14,18 +15,6 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
   def is_logged_in?
     session[:user_id].present?
-  end
-
-  def log_in_as(user)
-    session[:user_id] = user.id
-  end
-end
-
-class ActionDispatch::IntegrationTest
-  def log_in_as(user, password: 'password', remember_me: '1')
-    post login_path, params: { session: { email: user.email,
-                                          password: password,
-                                          remember_me: remember_me } }
   end
 
   def log_in_as(user)
